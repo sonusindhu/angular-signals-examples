31cd3e41a262b48f2161e40afb9f9a5743d53fe3
Sonu Sindhu <sonusindhu10@yahoo.com>
2023-05-04 20:16:59 +0530

    added provideZoneChangeDetection to bootstrap app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app-routings';
+import { provideZoneChangeDetection } from '@angular/core';
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(ROUTES)],
+  providers: [
+    provideRouter(ROUTES),
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+      runCoalescing: true,
+    }),
+  ],
 })
   .then(() => {
     console.log('Standalone app initiazed');
