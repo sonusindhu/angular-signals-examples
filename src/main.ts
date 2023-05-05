a09ee6ab61db59f37e0d3ed53b8b3bd6bceb15b3
Sonu Sindhu <sonusindhu10@yahoo.com>
2023-05-05 18:37:37 +0530

    added material package

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app-routings';
 import { provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(ROUTES),
     provideZoneChangeDetection({
-      eventCoalescing: true,
-      runCoalescing: true,
+        eventCoalescing: true,
+        runCoalescing: true,
     }),
-  ],
+    provideAnimations()
+],
 })
   .then(() => {
     console.log('Standalone app initiazed');
