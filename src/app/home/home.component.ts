e4c8483d5b2275f1d85b16af8bada4e5dcc3f046
Sonu Sindhu <sonusindhu10@yahoo.com>
2023-05-06 11:57:12 +0530

    updated home page with the signal example card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+interface ExampleModel {
+  title: string;
+  content: string;
+  routerLink: string;
+}
+const EXAMPLES: ExampleModel[] = [
+  {
+    title: 'Example 1',
+    content: 'Count Increment/Decrement',
+    routerLink: '/signal/signal-example1',
+  },
+  {
+    title: 'Example 2',
+    content: 'Auto increment count',
+    routerLink: '/signal/signal-example2',
+  },
+  {
+    title: 'Example 3',
+    content: 'Update name using the input',
+    routerLink: '/signal/signal-example3',
+  },
+  {
+    title: 'Example 4',
+    content: 'Computed signal value',
+    routerLink: '/signal/signal-example4',
+  },
+];
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  examples = signal(EXAMPLES);
+}
