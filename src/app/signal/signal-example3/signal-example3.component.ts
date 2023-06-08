bb7e2ce34dfdad805b609c913cac89026ee0af5a
Sonu Sindhu <sonusindhu10@yahoo.com>
2023-06-08 20:08:40 +0530

    fixed Example 3 - add material input #8

--- src/app/signal/signal-example3/signal-example3.component.ts
+++ src/app/signal/signal-example3/signal-example3.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-signal-example3',
   standalone: true,
   templateUrl: './signal-example3.component.html',
   styleUrls: ['./signal-example3.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatFormFieldModule, MatInputModule]
 })
 export default class SignalExample3Component {
   name = signal('Sonu Sindhu');
