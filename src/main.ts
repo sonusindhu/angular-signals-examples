4ea551d2b05f6c207bf90b61cf9ed3a7f1c6c574
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-03-01 19:11:35 +0530

    zoneless app

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app-routings';
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection, ɵprovideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(ROUTES),
-    provideZoneChangeDetection({
-        eventCoalescing: true,
-        runCoalescing: true,
-    }),
+    ɵprovideZonelessChangeDetection(),
     provideAnimations()
 ],
 })
