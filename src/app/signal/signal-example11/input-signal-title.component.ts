570b4e66f52d059a12c00087503f73b96748ea9f
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-03-02 12:37:05 +0530

    more updates

--- src/app/signal/signal-example11/input-signal-title.component.ts
+++ src/app/signal/signal-example11/input-signal-title.component.ts
@@ -2,16 +2,17 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  Signal,
-  WritableSignal,
+  signal,
 } from '@angular/core';
 
 @Component({
   selector: 'input-signal-title',
   standalone: true,
-  template: ` <p>Username is : <b>{{ title() }}</b> </p>`,
+  template: ` <p>
+    Username is : <b>{{ title() }}</b>
+  </p>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InputSignalComponent {
-  @Input() title: Signal<string>;
+  @Input() title = signal('');
 }
