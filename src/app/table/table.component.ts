43f9805f14f1d67d42dd94103a48ea30fc8c0732
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-03-16 15:40:34 +0530

    material table

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,300 @@
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ResizeColumnDirective } from './resize-column.directive';
+
+/**
+ * @title Table with expandable rows
+ */
+@Component({
+  selector: 'app-table',
+  styleUrl: 'table.component.scss',
+  templateUrl: 'table.component.html',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatSortModule,
+    MatMenuModule,
+    MatButtonModule,
+    DragDropModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ResizeColumnDirective,
+  ],
+})
+export default class TableExpandableRowsExample {
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  columnsToDisplayWithExpand = [
+    'select',
+    ...this.columnsToDisplay,
+    'expand',
+    'option',
+  ];
+  expandedElement: PeriodicElement | null = null;
+  hiddenColumns: string[] = [];
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  constructor(
+    private _liveAnnouncer: LiveAnnouncer,
+    private renderer: Renderer2
+  ) {
+    this.dataSource.filterPredicate = this.createFilter();
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @ViewChild(MatTable, { read: ElementRef }) private matTableRef!: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.sort);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: any) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  drop(event: any) {
+    // CdkDragDrop<string[]>
+    moveItemInArray(
+      this.columnsToDisplay,
+      event.previousIndex,
+      event.currentIndex
+    );
+
+    this.columnsToDisplayWithExpand = [
+      'select',
+      ...this.columnsToDisplay,
+      'expand',
+      'option',
+    ];
+  }
+
+  filteredValues: any = { name: '', position: '', weight: '', symbol: '' };
+  applyFilter(event: Event, column: any) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    const searchText = filterValue.trim().toLowerCase();
+    this.filteredValues = { ...this.filteredValues, [column]: searchText };
+    console.log(this.filteredValues);
+    this.dataSource.filter = Object.keys(this.filteredValues).length
+      ? JSON.stringify(this.filteredValues)
+      : '';
+    console.log(this.dataSource.filter);
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  createFilter(): (data: any, filter: string) => boolean {
+    let filterFunction = function (data: any, filter: any): boolean {
+      let searchTerms = JSON.parse(filter);
+      console.log(searchTerms, data);
+      return (
+        data.name.toLowerCase().includes(searchTerms.name) &&
+        data.weight.toString().toLowerCase().includes(searchTerms.weight) &&
+        data.symbol.toLowerCase().includes(searchTerms.symbol) &&
+        data.position.toString().toLowerCase().includes(searchTerms.position)
+      );
+    };
+    return filterFunction;
+  }
+
+  toggleColumn(column: any) {
+    if (!this.hiddenColumns.includes(column)) {
+      this.hiddenColumns.push(column);
+    } else {
+      this.hiddenColumns = this.hiddenColumns.filter((c) => c != column);
+    }
+
+    this.columnsToDisplayWithExpand = [
+      'select',
+      ...this.columnsToDisplay.filter((i) => !this.hiddenColumns.includes(i)),
+      'expand',
+      'option',
+    ];
+  }
+}
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H',
+    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+  },
+  {
+    position: 2,
+    name: 'Helium',
+    weight: 4.0026,
+    symbol: 'He',
+    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
+        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+        group in the periodic table. Its boiling point is the lowest among all the elements.`,
+  },
+  {
+    position: 3,
+    name: 'Lithium',
+    weight: 6.941,
+    symbol: 'Li',
+    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
+        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
+        lightest solid element.`,
+  },
+  {
+    position: 4,
+    name: 'Beryllium',
+    weight: 9.0122,
+    symbol: 'Be',
+    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
+        relatively rare element in the universe, usually occurring as a product of the spallation of
+        larger atomic nuclei that have collided with cosmic rays.`,
+  },
+  {
+    position: 5,
+    name: 'Boron',
+    weight: 10.811,
+    symbol: 'B',
+    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
+        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
+        low-abundance element in the Solar system and in the Earth's crust.`,
+  },
+  {
+    position: 6,
+    name: 'Carbon',
+    weight: 12.0107,
+    symbol: 'C',
+    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
+        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
+        to group 14 of the periodic table.`,
+  },
+  {
+    position: 7,
+    name: 'Nitrogen',
+    weight: 14.0067,
+    symbol: 'N',
+    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
+        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
+  },
+  {
+    position: 8,
+    name: 'Oxygen',
+    weight: 15.9994,
+    symbol: 'O',
+    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
+         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
+         agent that readily forms oxides with most elements as well as with other compounds.`,
+  },
+  {
+    position: 9,
+    name: 'Fluorine',
+    weight: 18.9984,
+    symbol: 'F',
+    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
+        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
+        conditions.`,
+  },
+  {
+    position: 10,
+    name: 'Neon',
+    weight: 20.1797,
+    symbol: 'Ne',
+    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
+        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
+        two-thirds the density of air.`,
+  },
+];
