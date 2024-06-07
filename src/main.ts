faa7b06b0a4de12fb3541fc82d1fb63f78df79d0
Sonu Sindhu <sonusindhu10@yahoo.com>
2024-06-07 19:35:38 +0530

    updated to provideZoneChangeDetection

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { ROUTES } from './app-routings';
-import { ɵprovideZonelessChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(ROUTES),
-    ɵprovideZonelessChangeDetection(),
+    provideZoneChangeDetection(),
     provideAnimations()
 ],
 })
