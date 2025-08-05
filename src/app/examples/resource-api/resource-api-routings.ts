ecae24b966a45938ef1a173950757c20493fd3cd
Sonu Sindhu <sonusindhu10@yahoo.com>
2025-08-05 20:22:26 +0530

    setup routes for more resource examples

--- src/app/examples/resource-api/resource-api-routings.ts
+++ src/app/examples/resource-api/resource-api-routings.ts
@@ -4,21 +4,23 @@ import ResourceApiComponent from './resource-api.component';
 const RESOURCE_API_ROUTES: Route[] = [
   {
     path: '',
-    component: ResourceApiComponent,
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'resource-api-example1',
-      },
-      {
-        path: 'resource-api-example1',
-        loadComponent: () =>
-          import('./resource-api-example1/resource-api-example1.component').then(x => x.ResourceApiExample1Component),
-      },
-      
-    ],
+    pathMatch: 'full',
+    loadComponent: () => import('./resource-api.component'),
   },
-];
-
+  {
+    path: 'resource-api-example1',
+    loadComponent: () =>
+      import('./resource-api-example1/resource-api-example1.component').then(x => x.ResourceApiExample1Component),
+  },
+  {
+    path: 'resource-api-example2',
+    loadComponent: () =>
+      import('./resource-api-example2/resource-api-example2.component').then(x => x.ResourceApiExample2Component),
+  },
+  {
+    path: 'resource-api-example3',
+    loadComponent: () =>
+      import('./resource-api-example3/resource-api-example3.component').then(x => x.ResourceApiExample3Component),
+  },
+]
 export default RESOURCE_API_ROUTES;
