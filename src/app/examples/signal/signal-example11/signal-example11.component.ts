a810d0fa6e503a47d175dd8033e41b6a36bdf988
sonusindhu <sonusindhu10@gmail.com>
2025-08-17 19:22:19 +0530

    feat(signal-example11): enhance example with input signal, improved layout, and styling

--- src/app/examples/signal/signal-example11/signal-example11.component.ts
+++ src/app/examples/signal/signal-example11/signal-example11.component.ts
@@ -1,3 +1,5 @@
+import { MatTabsModule } from '@angular/material/tabs';
+import { MarkdownComponent } from 'ngx-markdown';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,8 +8,9 @@ import { InputSignalComponent } from './input-signal-title.component';
 @Component({
     selector: 'app-signal-example11',
     templateUrl: './signal-example11.component.html',
+    styleUrl: './signal-example11.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatFormFieldModule, MatInputModule, InputSignalComponent]
+    imports: [MatFormFieldModule, MatInputModule, InputSignalComponent, MatTabsModule, MarkdownComponent]
 })
 export default class SignalExample11Component {
   searchInput = signal('');
