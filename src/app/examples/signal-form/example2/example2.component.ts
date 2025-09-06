b23b1628025bf654644047d90dcf28df9f17d697
sonusindhu <sonusindhu10@gmail.com>
2025-09-06 02:11:01 -0400

    feat: add example 2 component with form validation and navigation

---
+++ src/app/examples/signal-form/example2/example2.component.ts
@@ -0,0 +1,50 @@
+import { Component, effect, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MarkdownComponent } from 'ngx-markdown';
+import { RouterModule } from '@angular/router';
+
+import { form, Control, required, pattern, email, schema } from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
+
+
+const formSchema = schema<{name: string, email: string}>((form) => {
+  required(form.name, { message: 'please enter your name' });
+  pattern(form.name, /^[a-z ]+$/i, { message: 'Enter valid name' });
+
+
+  required(form.email, { message: 'please enter your email' });
+  email(form.email, { message: 'Enter valid email' });
+});
+
+@Component({
+  selector: 'app-form-example1',
+  imports: [
+    MatButtonModule, 
+    MatTabsModule, 
+    MatCardModule, 
+    MatIconModule, 
+    MarkdownComponent, 
+    RouterModule, 
+    Control,
+    JsonPipe,
+  ],
+  templateUrl: './example2.component.html',
+  styleUrl: './example2.component.scss'
+})
+export class FormExample2Component {
+
+  // create a signal for the user form
+  public user = signal({ name: '', email: '' });
+
+  public errors = effect(() => {
+    console.log('errors - ', this.userForm().errors())
+    return this.userForm().errors();
+  });
+
+  // create a form using the user signal
+  public userForm = form(this.user, formSchema);
+
+}
