d4e4c0dc68cdc2250d889347fbade8c40e9cb8b6
sonusindhu <sonusindhu10@gmail.com>
2025-09-07 03:10:00 -0400

    feat: add Example 3 component with custom validation and form handling

---
+++ src/app/examples/signal-form/example3/example3.component.spec.ts
@@ -0,0 +1,7 @@
+import { FormExample3Component } from './example3.component';
+describe('FormExample3Component', () => {
+  it('should create', () => {
+    const component = new FormExample3Component();
+    expect(component).toBeTruthy();
+  });
+});
